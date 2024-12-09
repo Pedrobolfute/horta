d6ba00730e054e459a1926e0ca20c66c4541f63f
pedrobolfute <pepenike-101@hotmail.com>
2024-12-08 21:39:30 -0300

    first commit

---
+++ login/login/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', include('app_login.urls')),
+    path('register/', include('app_create_account.urls')),
+    path('change/', include('app_change_account.urls')), #Mudar para formulario
+    path('welcome/', include('app_welcome.urls')),
+    path('administrator/', include('app_admin.urls')),
+]
